af1ca16e55deef41efbf0e66ba05c9dcc201ebb3
Kent Ian Ramirez <160685397+KentRamriez@users.noreply.github.com>
2024-08-21 09:54:36 +0800

    Welcome

--- RamirezKentIanCS2101_OOPActivities/Exercise_Set01/Welcome.java
+++ RamirezKentIanCS2101_OOPActivities/Exercise_Set01/Welcome.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class Welcome {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Welcome to CS 211: Object-Oriented Programming!");
+        System.out.println("This course will be fun, " + name + "!");
+        scanner.close();
+    }
 }
